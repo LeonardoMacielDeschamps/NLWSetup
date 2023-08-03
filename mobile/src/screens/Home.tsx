0736b22f72fea07d378f67090eacd465d972a2ec
Leonardo Maciel Deschamps <leonardomacieldeschamps@gmail.com>
2023-08-03 20:37:52 -0300

    Aula 02

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,9 +1,14 @@
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
+
+import { generateRangeDatesFromYearStart } from '../utils/generate-range-between-dates';
 
 import { Header } from '../components/Header';
 import { HabitDay, DAY_SIZE } from '../components/HabitDay';
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+const datesFromYearStart = generateRangeDatesFromYearStart();
+const minimumSummaryDatesSize = 18 * 7;
+const amountOfDaysToFill = minimumSummaryDatesSize - datesFromYearStart.length;
 
 export function Home() {
   return (
@@ -24,7 +29,33 @@ export function Home() {
         }
       </View>
 
-      <HabitDay />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className='flex-row flex-wrap'>
+          {
+            datesFromYearStart.map(date => (
+              <HabitDay
+                key={date.toISOString()}
+              />
+            ))
+          }
+          {
+            Array.from({ length: amountOfDaysToFill }).map((_, index) => (
+              <View
+                key={index}
+                className='bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40'
+                style={{
+                  width: DAY_SIZE,
+                  height: DAY_SIZE
+                }}
+              />
+            )
+            )
+          }
+        </View>
+      </ScrollView>
     </View>
   );
 }
