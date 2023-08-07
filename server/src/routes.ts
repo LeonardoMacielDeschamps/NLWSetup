b1037b3c1b3f4b3b648778b035da12864dfd456d
Leonardo Maciel Deschamps <leonardomacieldeschamps@gmail.com>
2023-08-06 21:47:58 -0300

    Aula 03

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -70,4 +70,80 @@ export async function appRoutes(app: FastifyInstance) {
       completedHabits
     };
   });
+
+  app.patch('/habit/:id/toggle', async (request) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid()
+    });
+
+    const { id } = toggleHabitParams.parse(request.params);
+
+    const today = dayjs().startOf('day').toDate();
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today
+      }
+    });
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today
+        }
+      });
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        dayId_habitId: {
+          dayId: day.id,
+          habitId: id
+        }
+      }
+    });
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id
+        }
+      });
+    }
+    else {
+      await prisma.dayHabit.create({
+        data: {
+          dayId: day.id,
+          habitId: id
+        }
+      });
+    }
+  });
+
+  app.get('/summary', async () => {
+    const summary = await prisma.$queryRaw`
+      SELECT
+        day.id,
+        day.date,
+        (
+          SELECT
+            cast(count(*) AS float)
+          FROM day_habit
+          WHERE day_habit.dayId = day.id
+        ) AS completed,
+        (
+          SELECT
+            cast(count(*) AS float)
+          FROM habit_week_day
+          JOIN habit
+            ON habit.id = habit_week_day.habitId
+          WHERE
+            habit_week_day.weekDay = cast(strftime('%w', day.date/1000.0, 'unixepoch') AS int)
+            AND habit.createdAt <= day.date
+        ) AS amount
+      FROM day
+    `;
+
+    return summary;
+  });
 }
